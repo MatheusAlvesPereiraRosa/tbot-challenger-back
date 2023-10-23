25f1be9716d4d6d3da8a9e0bbab28ba34480d348
Matheus Alves <matpr.a98@gmail.com>
2023-10-23 09:18:47 -0300

    Recebendo e enviando mensagens por chatId

--- routes/api.js
+++ routes/api.js
@@ -8,19 +8,60 @@ const router = express.Router();
 // Criando um instância do bot
 const botInstance = new TelegramBot();
 
+router.get('/messages/:chatId', async (req, res) => {
+  const chatId = req.params.chatId;
+
+  try {
+    // Query your database to retrieve messages with the provided chatId
+    const messages = await Message.find({ chatId });
+
+    // Send the retrieved messages as a response
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error('Error fetching messages:', error);
+    res.status(500).send('Failed to fetch messages');
+  }
+});
+
+router.get('/getHistory', async (req, res) => {
+  await Message.find().sort({ timestamp: -1 })
+    .then((messages) => {
+      res.json(messages)
+      console.log('Retrieved messages:', messages);
+    })
+    .catch((error) => {
+      console.error('Error while retrieving messages:', error);
+      res.status(500).send('Failed to retrieve the messages:' + error)
+    });
+})
+
+router.get('/getChats', async (req, res) => {
+  await Chat.find()
+    .then((chats) => {
+      res.json(chats)
+      console.log('Retrieved chats:', chats);
+    })
+    .catch((error) => {
+      console.error('Error while retrieving messages:', error);
+      res.status(500).send('Failed to retrieve the messages:' + error)
+    });
+})
+
 router.post('/sendMessage', async (req, res) => {
   const messageText = req.body.message
   const timestamp = req.body.timestamp
+  const chatId = req.body.chatId
 
   try {
     // Usando a instância do bot para mandar mensagens
-    await botInstance.bot.telegram.sendMessage(6186971422, messageText);
+    await botInstance.bot.telegram.sendMessage(chatId, messageText);
 
     const userId = req.body.userId;
 
     const newMessage = new Message({
       text: messageText,
       userId,
+      chatId,
       timestamp: timestamp, // Campo de tempo para saber quando a mensagem foi enviada
       isUserMessage: true, // Campo booleano para distinguir se é o usuário ou o bot
     });
@@ -40,17 +81,5 @@ router.post('/sendMessage', async (req, res) => {
     res.status(500).send('Failed to send message');
   }
 });
-
-router.get('/getHistory', async (req, res) => {
-  await Message.find().sort({ timestamp: -1 })
-    .then((messages) => {
-      res.json(messages)
-      console.log('Retrieved messages:', messages);
-    })
-    .catch((error) => {
-      console.error('Error while retrieving messages:', error);
-      res.status(500).send('Failed to retrieve the messages:' + error)
-    });
-})
 
 module.exports = router;
