6b521ba5ce46a55e5301b737d01af8436580af7a
Matheus Alves <matpr.a98@gmail.com>
2023-10-24 15:53:38 -0300

    Padronizando funções de mensagens

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,16 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/usersController');
+const authenticateUser = require('../middleware/userAuth');
 
 // Define routes for user registration and login
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
+router.post('/resetPassword', userController.resetPassword);
+router.post('/changePassword', userController.changePassword);
+router.get('/protected', authenticateUser, (req, res) => {
+  res.json({ message: 'You have access to this protected route' });
+});
+router.get('/logout', userController.logout)
 
 module.exports = router;
